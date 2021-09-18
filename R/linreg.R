5580fa793dcb0d7e3011a1e04ac1b4d1e1980f9f
Elisio Quintino <elife599@student.liu.se>
2021-09-19 00:47:35 +0200

    added some comments

--- R/linreg.R
+++ R/linreg.R
@@ -1,10 +1,10 @@
 data("iris")
 
 
-my_formula = Petal.Length ~ Sepal.Width + Sepal.Length
+my_formula = Petal.Length ~ Sepal.Width + Sepal.Length #establishes independent and dependent variables
 data = iris
 ###
-X = model.matrix(object = my_formula, data = data)
-variable_names = all.vars(my_formula)
-y = data[[variable_names[which(!(all.vars(my_formula) %in% colnames(X)))]]]
-coef = solve((t(X) %*% X), (t(X) %*% y))
+X = model.matrix(object = my_formula, data = data) #extracts the model matrix, that is, the matrix originated from writting the problem as a set of linear equations
+variable_names = all.vars(my_formula) #all variables names in the formula
+y = data[[variable_names[which(!(all.vars(my_formula) %in% colnames(X)))]]] #picks the name from the formula that is not in the columns of X and identifies it as the name of y. picks y from the data with this name
+coef = solve((t(X) %*% X), (t(X) %*% y)) #solves X'X coef = X' y
