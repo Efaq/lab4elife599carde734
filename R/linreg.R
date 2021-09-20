5ded1fe648f4da86e1e0dd7136a65e22bdf7595e
Carolina Rosario <carde734@student.liu.se>
2021-09-21 01:08:15 +0200

    update

--- R/linreg.R
+++ R/linreg.R
@@ -46,10 +46,50 @@ linreg = setRefClass(
       #TODO
     },
     plot = function(){
-      #TODO
+      
+      labels_plot_1 <- vector(length=150)
+      labels_plot_2 <- vector(length=150)
+      labels_plot_1[] <- ""
+      labels_plot_2[] <- ""
+      labels_plot_1[119]<-'119'
+      labels_plot_1[118]<-'118'
+      labels_plot_2[99]<-'99'
+      
+      
+      
       stand = sqrt(abs((residuals/sd(residuals))))
-      #ggplot(data = data.frame(y_est, residuals)) + geom_point(mapping = aes(x = y_est, y = residuals))
-      ggplot(data = data.frame(y_est, stand)) + geom_point(mapping = aes(x = y_est, y = stand))
+      
+      
+      p1<- ggplot(data = data.frame(y_est, residuals)) + 
+        geom_point(mapping=aes(x=y_est, y=residuals),shape=21,size=2.5) + 
+        labs(title = "Residuals vs Fitted",
+             x=paste("Fitted values \n linreg(", deparse(local_formula),")", sep = "") ,
+             y="Residuals")+
+        theme(plot.title = element_text(hjust = 0.5, size=12)) +
+        geom_text(mapping=aes(x=y_est, y=residuals),label=ifelse(labels_plot_1 != FALSE ,as.character(labels_plot_1),''),hjust=1.5,vjust=0.5) +
+        geom_text(mapping=aes(x=y_est, y=residuals),label=ifelse(labels_plot_2 != FALSE ,as.character(labels_plot_2),''),hjust=-0.5,vjust=0.5) +
+        theme(panel.background = element_blank(), element_line(colour = "black"),panel.border = element_rect(colour = "black", fill=NA, size=1))+
+        stat_summary(mapping=aes(x=y_est, y=residuals),fun = median, geom="line", colour="red")+
+        geom_hline(yintercept=-0, linetype="dotted", color = "grey", size=1)+
+        scale_y_continuous(limits=c(-1.5, 1.5), breaks = seq(-1.5, 1.5, by=1)) 
+      
+      
+      
+      
+      p2<- ggplot(data = data.frame(y_est, stand)) + 
+        geom_point(mapping=aes(x=y_est, y=stand),shape=21,size=2.5) + 
+        labs(title = "Scale - Location",
+             x=paste("Fitted values \n linreg(", deparse(local_formula),")", sep = "") ,
+             y=expression(sqrt("|Standardized Residuals|")))+
+        theme(plot.title = element_text(hjust = 0.5, size=12)) +
+        geom_text(mapping=aes(x=y_est, y=stand),label=ifelse(labels_plot_1 != FALSE ,as.character(labels_plot_1),''),hjust=1.5,vjust=0.5) +
+        geom_text(mapping=aes(x=y_est, y=stand),label=ifelse(labels_plot_2 != FALSE ,as.character(labels_plot_2),''),hjust=-0.5,vjust=0.5) +
+        theme(panel.background = element_blank(), element_line(colour = "black"),panel.border = element_rect(colour = "black", fill=NA, size=1))+
+        stat_summary(mapping=aes(x=y_est, y=stand),fun = mean, geom="line", colour="red") 
+      
+      plist<-list(p1,p2)
+      return(plist)
+      
     }
   )
 )
