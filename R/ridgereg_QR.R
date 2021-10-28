9b7976a4f5afc06754e9cc06a01ef099238bebb1
Elisio Quintino <elife599@student.liu.se>
2021-10-28 22:29:41 +0200

    Cutting lines in doc

--- R/ridgereg_QR.R
+++ R/ridgereg_QR.R
@@ -14,7 +14,9 @@
 #'
 #' @examples
 #' data(iris)
-#'regression_object <- ridgereg_QR(formula=Petal.Width ~ Sepal.Length + Sepal.Width + Petal.Length, data = iris, lambda=1)
+#'regression_object <- ridgereg_QR(
+#'   formula=Petal.Width ~ Sepal.Length + Sepal.Width + Petal.Length,
+#'   data = iris, lambda=1)
 #'regression_object$predict()
 #'regression_object$coef()
 #'regression_object$print()
