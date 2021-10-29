ca3d7dd176f6e38500ef041d17cb4aa87711a3b8
Carolina Rosario <carde734@student.liu.se>
2021-10-29 12:59:25 +0100

    update airports

--- R/visualize_airport_delays.R
+++ R/visualize_airport_delays.R
@@ -1,63 +1,40 @@
 #'Visualize airport delays
 #'
-#'\code{linreg} performs linear regression, stores its result and offers methods for analysis.
+#'\code{visualize_airport_delays} returns plots concerning the mean delay of flights for different airports by longitude and latitude. The data used is from nycflights13 package.
 #'
-#'\code{linreg} is an RC class. Upon object instantiation, in the \code{initialize} method, it performs
-#'all calculations of quantities of interest, namely estimated coefficients, fitted dependent variable values,
-#'residuals, degrees of freedom of the system, residual variance, the variance of the coefficients,
-#'t-values and p-values for each coefficient.
-#'
-#'Residuals, predicted values and coefficients can be obtained through the methods \code{resid()}, \code{pred()} and \code{coef()}.
-#'
-#'\code{print()} prints out the input information used in the regression and the resulting coefficients.
-#'
-#'\code{summary()} prints out statistics about the regression.
-#'
-#'\code{plot()} plots interesting graphics about the performed regression.
 #'
 #' @examples
-#' data(iris)
-#' regression_object = linreg(Petal.Length~Species, data = iris)
-#' regression_object$resid()
-#' regression_object$pred()
-#' regression_object$coef()
-#' regression_object$summary()
-#' 
-#' @source 
-#' Read more at \url{https://en.wikipedia.org/wiki/Linear_regression}
+#' visualize_airport_delays()
 #'
 #' @importFrom methods new
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 geom_point
 #' @importFrom ggplot2 labs
 #' @importFrom ggplot2 theme
+#' @importFrom ggplot2 geom_label
 #' @importFrom ggplot2 geom_text
-#' @importFrom ggplot2 stat_summary
-#' @importFrom ggplot2 geom_hline
-#' @importFrom ggplot2 scale_y_continuous
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 element_text
 #' @importFrom ggplot2 element_rect
 #' @importFrom ggplot2 element_line
 #' @importFrom ggplot2 element_blank
-#' @export linreg
-
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate
+#' @importFrom dplyr left_join
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr summarise
+#' @importFrom dplyr arrange
+#' @importFrom dplyr distinct
+#' @import nycflights13
+#' @usage data(flights)
+#' @usage data(airports)
+#' @export visualize_airport_delays
 
 
 visualize_airport_delays <- function () 
-{
-  install.packages("nycflights13")
-  data(package = "nycflights13")  
-  install.packages("dplyr")
-  install.packages("ggplot2")
-  library(ggplot2)
-  library(dplyr)
-  
-  
-  data("flights", package = "nycflights13")  
-  
-  data("airports", package = "nycflights13")  
-  
+{ 
+
   #get only the columns that we need of flights - origins
   flights_treated_ori <- select(flights, origin, dep_delay)
   
@@ -90,7 +67,9 @@ visualize_airport_delays <- function ()
   
   #calculte delay mean
   
-  group_ori_mean <- joined_data_ori2 %>% group_by(origin) %>% summarise(avg_delay =mean(dep_delay) )
+  group_ori_mean <- joined_data_ori2 %>%
+    group_by(origin) %>%
+    summarise(avg_delay = mean(dep_delay) )
   
   group_dest_mean <- joined_data_dest2 %>% group_by(dest) %>% summarise(avg_delay =mean(arr_delay) )
   
@@ -104,15 +83,15 @@ visualize_airport_delays <- function ()
   destination_delays_head<-head(arrange(destination_delays,desc(avg_delay) ))
   
   
-
+  
   p1<- ggplot(data = as.data.frame(origin_delays)) + 
     geom_point(mapping=aes(x=lat, y=lon)) + 
     labs(title = "Flights Delay - Origin",
          x="Latitude",
          y="Longitude")  +
     theme_minimal() + 
-     geom_label(mapping=aes(x=lat, y=lon), label=c(origin_delays)[[3]]
-     ) 
+    geom_label(mapping=aes(x=lat, y=lon), label=c(origin_delays)[[3]]
+    ) 
   
   
   p2<- ggplot(data= as.data.frame(origin_delays), aes(x= paste(c(origin_delays)[[3]]," - " ,c(origin_delays)[[4]]), y=avg_delay )) +
@@ -143,11 +122,8 @@ visualize_airport_delays <- function ()
   
   
   
-  
   plist<-list(p1,p2,p3,p4)
   return(plist)
   
- 
-  
-  
+
 }
